d414447fdce4029872a07632a941a2d80e4368e9
JoseAugusto05 <jose.augusto.sf.2405@gmail.com>
2023-02-18 10:53:48 -0300

    added django.admin

--- recipes/views.py
+++ recipes/views.py
@@ -10,5 +10,6 @@ def home(request):
 
 
 def recipe(request, id):
-    context = {'recipe': [make_recipe()]}
+    context = {'recipe': [make_recipe()],
+               'is_detail_page': True, }
     return render(request, 'recipes/pages/recipe-view.html', context=context)
