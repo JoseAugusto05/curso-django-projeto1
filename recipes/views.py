21af411d0a47c675f758061f8f48c4745585d5d4
JoseAugusto05 <jose.augusto.sf.2405@gmail.com>
2023-02-24 11:00:06 -0300

    improved the framework usage

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -26,7 +29,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
