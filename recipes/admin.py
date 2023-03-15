507d40ded770ccfbf2d443256b7968772cb79c4c
JoseAugusto05 <jose.augusto.sf.2405@gmail.com>
2023-03-15 09:43:02 -0300

    django admin custom.

--- recipes/admin.py
+++ recipes/admin.py
@@ -9,7 +9,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'title', 'author', 'created_at', 'is_published',
+    list_display_links = 'title', 'author',
+    search_fields = 'id', 'title', 'description', 'slug', 'preparation_steps',
+    list_filter = 'category', 'author', 'is_published',
+    list_per_page = 10
+    list_editable = 'is_published',
 
 
 admin.site.register(Category, CategoryAdmin)
