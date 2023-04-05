f05dcac1066b8caa1f09ef77d1dd8115c076539c
JoseAugusto05 <jose.augusto.sf.2405@gmail.com>
2023-04-05 13:39:35 -0300

    crud for deleting a recipe

--- authors/urls.py
+++ authors/urls.py
@@ -21,6 +21,11 @@ urlpatterns = [
         views.dashboard_recipe_edit,
         name='dashboard_recipe_edit'
     ),
+    path(
+        'dashboard/recipe/<int:id>/delete/',
+        views.dashboard_recipe_delete,
+        name='dashboard_recipe_delete'
+    ),
 
 
 ]
